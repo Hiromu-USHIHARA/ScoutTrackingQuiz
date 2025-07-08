ad0485372d9228514607f350e9395e2f1c06fccb
Hiromu Ushihara <h.ushihara223@gmail.com>
2025-07-08 21:52:45 +0900

    画像のディレクトリを変更し，ファイルを整理

--- src/data/questions.js
+++ src/data/questions.js
@@ -1,19 +1,20 @@
-import toRight1 from '../assets/images/toRight1.png';
-import toRight2 from '../assets/images/toRight2.png';
-import toRight3 from '../assets/images/toRight3.png';
-import toRight4 from '../assets/images/toRight4.png';
-import toLeft1 from '../assets/images/toLeft1.png';
-import toLeft2 from '../assets/images/toLeft2.png';
-import toLeft3 from '../assets/images/toLeft3.png';
-import toLeftNear1 from '../assets/images/toLeftNear1.png';
-import thisWay1 from '../assets/images/thisWay1.png';
-import thisWay2 from '../assets/images/thisWay2.png';
-import notThisWay1 from '../assets/images/notThisWay1.png';
-import notThisWay2 from '../assets/images/notThisWay2.png';
-import danger1 from '../assets/images/danger1.png';
-import danger2 from '../assets/images/danger2.png';
-import goBack from '../assets/images/goBack.png';
-import message3step from '../assets/images/message3step.png';
+import toRight1 from '../assets/toRight1.png';
+import toRight2 from '../assets/toRight2.png';
+import toRight3 from '../assets/toRight3.png';
+import toRight4 from '../assets/toRight4.png';
+import toRight5 from '../assets/toRight5.png';
+import toLeft1 from '../assets/toLeft1.png';
+import toLeft2 from '../assets/toLeft2.png';
+import toLeft3 from '../assets/toLeft3.png';
+import toLeftNear1 from '../assets/toLeftNear1.png';
+import thisWay1 from '../assets/thisWay1.png';
+import thisWay2 from '../assets/thisWay2.png';
+import notThisWay1 from '../assets/notThisWay1.png';
+import notThisWay2 from '../assets/notThisWay2.png';
+import danger1 from '../assets/danger1.png';
+import danger2 from '../assets/danger2.png';
+import goBack from '../assets/goBack.png';
+import message3step from '../assets/message3step.png';
 
 export const questions = [
   {
@@ -35,6 +36,11 @@ export const questions = [
     image: toRight4,
     correctAnswer: '右に進め',
     options: ['右に進め', '3歩先に通信あり', '左に進め']
+  },
+  {
+    image: toRight5,
+    correctAnswer: '右に進め',
+    options: ['右に進め', 'この方向には進むな', '左に進め']
   },
   {
     image: toLeft1,
